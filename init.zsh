61e68ed04ec84aaaee0b6d6a51cab06581af592d
Philip M. Gollucci <pgollucci@p6m7g8.com>
2024-03-24 12:34:22 -0400

    chore: p6_alias (#2)

--- init.zsh
+++ init.zsh
@@ -11,99 +11,47 @@ p6df::modules::homebrew::deps() {
   )
 }
 
+######################################################################
+#<
+#
+# Function: p6df::modules::homebrew::aliases::init()
+#
+#>
+######################################################################
+p6df::modules::homebrew::aliases::init() {
+
+  p6_alias "p6_hbr" "p6df::modules::homebrew::util::remove"
+  p6_alias "p6_hbcr" "p6df::modules::homebrew::util::casks::remove"
+  p6_alias "p6_hbbr" "p6df::modules::homebrew::util::brew::remove"
+
+  p6_return_void
+}
+
 # Warning: Using vim because no editor was set in the environment.
 # This may change in the future, so we recommend setting EDITOR,
 # or HOMEBREW_EDITOR to your preferred text editor.
 ######################################################################
 #<
 #
-# Function: p6df::modules::homebrew::init()
+# Function: p6df::modules::homebrew::init(_module, dir)
+#
+#  Args:
+#	_module -
+#	dir -
 #
 #  Environment:	 HOMEBREW_EDITOR HOMEBREW_PREFIX PREFIX
 #>
 ######################################################################
 p6df::modules::homebrew::init() {
+  local _module="$1"
+  local dir="$2"
+
+  p6_bootstrap "$dir"
 
   local homebrew_prefix=$(brew --config | awk -F: '/PREFIX/ { print $2 }' | sed -e 's, ,,g')
 
   p6_env_export "HOMEBREW_EDITOR" "vim"
   p6_env_export "HOMEBREW_PREFIX" "$homebrew_prefix"
 
-  alias p6_hbr=p6df::modules::homebrew::remove
-  alias p6_hbcr=p6df::modules::homebrew::casks::remove
-  alias p6_hbbr=p6df::modules::homebrew::brew::remove
-}
-
-######################################################################
-#<
-#
-# Function: p6df::modules::homebrew::remove()
-#
-#>
-######################################################################
-p6df::modules::homebrew::remove() {
-
-  p6df::modules::homebrew::casks::remove
-  p6df::modules::homebrew::brews::remove
-}
-
-######################################################################
-#<
-#
-# Function: p6df::modules::homebrew::casks::remove()
-#
-#>
-######################################################################
-p6df::modules::homebrew::casks::remove() {
-
-  local formuli=$(brew list --cask)
-
-  local formula
-  for formula in $(echo $formuli); do
-    brew uninstall --cask $formula
-  done
-}
-
-######################################################################
-#<
-#
-# Function: p6df::modules::homebrew::brews::remove()
-#
-#>
-######################################################################
-p6df::modules::homebrew::brews::remove() {
-
-  local formuli=$(brew list --formula)
-
-  local formula
-  for formula in $(echo $formuli); do
-    brew uninstall --ignore-dependencies --force $formula
-  done
-}
-
-######################################################################
-#<
-#
-# Function: p6df::modules::homebrew::nuke()
-#
-#  Environment:	 HOMEBREW_PREFIX
-#>
-######################################################################
-p6df::modules::homebrew::nuke() {
-
-  rm -rf $HOMEBREW_PREFIX
-  mkdir -p $HOMEBREW_PREFIX
-}
-
-######################################################################
-#<
-#
-# Function: p6df::modules::homebrew::install()
-#
-#  Environment:	 HEAD
-#>
-######################################################################
-p6df::modules::homebrew::install() {
-
-  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+  p6_return_void
 }
---
+++ lib/util.sh
@@ -0,0 +1,75 @@
+# shellcheck shell=bash
+######################################################################
+#<
+#
+# Function: p6df::modules::homebrew::util::install()
+#
+#  Environment:	 HEAD
+#>
+######################################################################
+p6df::modules::homebrew::util::install() {
+
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+}
+
+######################################################################
+#<
+#
+# Function: p6df::modules::homebrew::util::nuke()
+#
+#  Environment:	 HOMEBREW_PREFIX
+#>
+######################################################################
+p6df::modules::homebrew::util::nuke() {
+
+    p6_dir_rmrf "$HOMEBREW_PREFIX"
+    p6_dir_mk "$HOMEBREW_PREFIX"
+}
+
+# shellcheck shell=bash
+######################################################################
+#<
+#
+# Function: p6df::modules::homebrew::util::remove()
+#
+#>
+######################################################################
+p6df::modules::homebrew::util::remove() {
+
+    p6df::modules::homebrew::util::casks::remove
+    p6df::modules::homebrew::util::brews::remove
+}
+
+######################################################################
+#<
+#
+# Function: p6df::modules::homebrew::util::casks::remove()
+#
+#>
+######################################################################
+p6df::modules::homebrew::util::casks::remove() {
+
+    local formuli=$(p6df::modules::homebrew::cmd::brew list --cask)
+
+    local formula
+    for formula in $(echo $formuli); do
+        p6df::modules::homebrew::cmd::brew uninstall --cask "$formula"
+    done
+}
+
+######################################################################
+#<
+#
+# Function: p6df::modules::homebrew::brews::remove()
+#
+#>
+######################################################################
+p6df::modules::homebrew::brews::remove() {
+
+    local formuli=$(brew list --formula)
+
+    local formula
+    for formula in $(echo $formuli); do
+        p6df::modules::homebrew::cmd::brew uninstall --ignore-dependencies --force "$formula"
+    done
+}
